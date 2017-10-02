de5dcbf29d6ebc30f89796fe55aa83166440e38f
Vanessa <vsnakahara@gmail.com>
2017-10-02 18:47:05 -0300

    assingment

--- auctions-data/src/auctions/AuctionsData.java
+++ auctions-data/src/auctions/AuctionsData.java
@@ -8,6 +8,24 @@ public class AuctionsData {
     
     public static void main(String[] args) {
         
+        Turmas turmas = new Turmas();
+        
+        Turmas T1 =  new Turmas("T1", 4, 0.00);
+        Turmas T2 =  new Turmas("T2", 6, 0.00);
+        Turmas T3 =  new Turmas("T3", 4, 0.00);
+        Turmas T4 =  new Turmas("T4", 6, 0.00);
+        Turmas T5 =  new Turmas("T5", 4, 0.00);
+        
+        turmas.getConjuntoS().put("S1", new Turmas("T1", 4, 0.00));
+        turmas.getConjuntoS().put("S1", new Turmas("T2", 6, 0.00));
+        turmas.getConjuntoS().put("S1", new Turmas("T3", 4, 0.00));
+        
+        turmas.getConjuntoS().put("S2", T4);
+        turmas.getConjuntoS().put("S2", T5);
+        
+        turmas.viewConjunto("S1");
+//        turmas.viewConjunto("S2");
+        
     }
     
 }
--- auctions-data/src/auctions/Turmas.java
+++ auctions-data/src/auctions/Turmas.java
@@ -1,6 +1,8 @@
 package auctions;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
     
 /**
  *
@@ -15,15 +17,43 @@ public class Turmas {
     
     private String nome;
     private int ch_turma;
+    private double valorEstimado;
     
     HashMap<String, Turmas> conjuntoS = new HashMap<>();
 
+    
+    public void viewConjunto(String chave){
+        for (Map.Entry<String, Turmas> entry : conjuntoS.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue().getNome());
+            if(entry.getKey().equals(chave)){
+                String key = entry.getKey();
+                Turmas value = entry.getValue();
+                
+            }
+        }
+    }
+   
+    
     public Turmas() {
         super();
+    }
+
+    public Turmas(String nome, int ch_turma, double valorEstimado) {
+        this.nome = nome;
+        this.ch_turma = ch_turma;
+        this.valorEstimado = valorEstimado;
     }
     
     
 
+    public double getValorEstimado() {
+        return valorEstimado;
+    }
+
+    public void setValorEstimado(double valorEstimado) {
+        this.valorEstimado = valorEstimado;
+    }
+    
     public String getNome() {
         return nome;
     }
